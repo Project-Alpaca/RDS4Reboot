0c8c0c9e2ceb9a6a70edaee31d9c5f12c268ce02
dogtopus <dogtopus@users.noreply.github.com>
2019-05-10 22:53:21 -0300

    Add a shortcut for importing

---
+++ src/RDS4-DS4.hpp
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+/** RDS4-DS4.hpp
+ *  Includes other necessary files for DS4 emulation.
+ *
+ *  Copyright 2019 dogtopus
+ */
+
+#pragma once
+
+#include "AuthenticatorDS4.hpp"
+#include "ControllerDS4.hpp"
+#include "TransportDS4.hpp"
